808dfa0a1a0be95efd0c6aad6296f45803a0800a
Ludofr3 <ludovic.de-chavagnac@epitech.eu>
2026-03-30 10:23:29 +0200

    fix(run.sh): build rust package with martin build and not develop

--- run.sh
+++ run.sh
@@ -4,16 +4,26 @@ set -e
 
 echo "🟢 Step 1 : Check the virtual environnement..."
 
-if [[ -z "$VIRTUAL_ENV" && -d ".venv" ]]; then
+if [-n "$VIRTUAL_ENV" ]; then
+    echo "   -> The virtual environnement is already active ($VIRTUAL_ENV)."
+elif [ -d ".venv" ]
     echo "   -> Any environnement detected. .venv activation..."
     source .venv/bin/activate
 else
-    echo "   -> The virtual environnement is already active ($VIRTUAL_ENV)."
+    echo "  -> No .venv detected. Using global system environement (CI mode)."
 fi
 
 echo "⚙️ Step 2 : Compilation of the Rust's librairy (nurbslib)..."
 cd nurbslib
-maturin develop
+
+if [ -n "$VIRTUAL_ENV" ]; then
+    maturin develop
+else
+    # NOTE: build the rust package (.so)
+    maturin build --release --out dist
+    # NOTE: Python can't ready .so file so the package is packaging into an archive standard file wheel (.whl)
+    pip install --break-system-packages dist/*.whl --force-reinstall
+fi
 cd ..
 
 echo "🚀 Step 3 : Launch of nurbs-convert..."
