b633f3c8d4d3e4723522f3690323f71e22e5d8de
Ludofr3 <ludovic.de-chavagnac@epitech.eu>
2026-03-30 16:39:44 +0200

    chore: delete example add function

--- nurbslib/src/lib.rs
+++ nurbslib/src/lib.rs
@@ -1,15 +1,8 @@
 #![allow(unsafe_op_in_unsafe_fn)]
 
-use core_rust::add as rust_add;
 use pyo3::prelude::*;
 
 pub mod geometry;
-
-#[pyfunction]
-#[pyo3(name = "add_numbers")]
-fn add(left: u64, right: u64) -> PyResult<u64> {
-    Ok(rust_add(left, right))
-}
 
 #[pymodule]
 fn nurbslib(m: &Bound<'_, PyModule>) -> PyResult<()> {
@@ -17,6 +10,5 @@ fn nurbslib(m: &Bound<'_, PyModule>) -> PyResult<()> {
         geometry::bezier::compute_knot_insertion_matrix,
         m
     )?)?;
-    m.add_function(wrap_pyfunction!(add, m)?)?;
     Ok(())
 }
